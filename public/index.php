8ea21e2a412006a50256173863863745cafe5635
Brian Tripp <btripp365@gmail.com>
2026-06-23 10:01:47 -0500

    chore: update resume

--- public/index.php
+++ public/index.php
@@ -1309,11 +1309,11 @@ startActivity(intent);
             <h3>Resume</h3>
             <div class="card">
                 <div id="icons">
-                    <a href="documents/brian_tripp_resume.pdf?v=062326" target="_blank">
+                    <a href="documents/brian_tripp_resume.pdf?v=062326-2" target="_blank">
                         <img id="pdf_icon" src="images/icons/pdf_icon.png">
                     </a>
                 </div>
-                <img src="documents/resume_preview.png?v=062326" alt="Resume preview">
+                <img src="documents/resume_preview.png?v=062326-2" alt="Resume preview">
             </div>
         </section>
         
